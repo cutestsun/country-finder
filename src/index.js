3a189b835c2b6eeb3c40cd44a771ec7647d60dad
Illia Shulha <ilyxa2836@gmail.com>
2023-04-22 17:34:05 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// import './css/styles.css';
+// import refs from './refs';
+// import debounce from 'lodash.debounce';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// refs.input.addEventListener('input', onInputSearch);
+// function onInputSearch(e) {
+//   const countryName = e.currentTarget.value;
+//   fetch(
+//     `https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`
+//   )
+//     .then(resolve => resolve.json())
+//     .then(r => console.log(r));
+// }
+
+// const DEBOUNCE_DELAY = 300;
